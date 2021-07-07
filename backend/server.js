ba5423682ac002f52fcc042382a63e515ece7c00
jay <agrawaljayprakash50@gmail.com>
2021-07-07 15:33:06 +0530

    final-add

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 // const bodyParser = require("body-parser");
 const PORT = 5000;
-const cors = require("cors");
+// const cors = require("cors");
 const axios = require("axios");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
